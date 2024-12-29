f72e4f07b9d03611b509959fd61eee28b68e99ff
José Ángel López <jose.a.l.e@hotmail.com>
2024-12-29 21:02:22 +0100

    feat: updates in project structure

---
+++ src/block.py
@@ -0,0 +1,20 @@
+import hashlib
+import time
+
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, data, nonce=0):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.nonce = nonce
+
+    def compute_hash(self):
+        block_string = f"{self.index}{self.previous_hash}{self.timestamp}{self.data}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+
+# tests
+block = Block(0, None, time.time(), "Genesis Block")
+print(block.compute_hash())
